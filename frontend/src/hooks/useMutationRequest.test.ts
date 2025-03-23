61045b33a87fbecada68b64eb063d23acf4670f7
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-03-23 15:24:28 -0400

    Write tests for useMutationRequest

---
+++ frontend/src/hooks/useMutationRequest.test.ts
@@ -0,0 +1,76 @@
+import { renderHook } from '@testing-library/react';
+import useMutationRequest from './useMutationRequest';
+import { backendUrl } from '../config';
+import { act } from 'react';
+
+describe('useMutationRequest', () => {
+  beforeEach(() => (globalThis.fetch = vi.fn()));
+
+  afterEach(() => vi.resetAllMocks());
+
+  afterAll(() => vi.restoreAllMocks());
+
+  it('should set the initial return values correctly', () => {
+    const { result } = renderHook(() =>
+      useMutationRequest<Record<string, string>, Record<string, string>>('dummy-endpoint', 'POST'),
+    );
+    expect(result.current.data).toBeNull();
+    expect(result.current.error).toBeNull();
+    expect(result.current.loading).toEqual(false);
+  });
+
+  it('should call fetch with the correct arguments', async () => {
+    const mockedFetch = globalThis.fetch as ReturnType<typeof vi.fn>;
+    const { result } = renderHook(() =>
+      useMutationRequest<{ field1: string; field2: string }, Record<string, string>>(
+        'dummy-endpoint',
+        'POST',
+      ),
+    );
+    await act(async () =>
+      result.current.sendRequest({ field1: 'some data', field2: 'other data' }),
+    );
+    expect(mockedFetch).toHaveBeenCalledExactlyOnceWith(`${backendUrl}/dummy-endpoint`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: `{"field1":"some data","field2":"other data"}`,
+    });
+  });
+
+  it('should correctly handle a successful response', async () => {
+    const mockedFetch = globalThis.fetch as ReturnType<typeof vi.fn>;
+    mockedFetch.mockResolvedValueOnce({ ok: true, json: async () => ({ message: 'success!' }) });
+    const { result } = renderHook(() =>
+      useMutationRequest<{ field1: string; field2: string }, Record<string, string>>(
+        'dummy-endpoint',
+        'POST',
+      ),
+    );
+    await act(async () =>
+      result.current.sendRequest({ field1: 'some data', field2: 'other data' }),
+    );
+    expect(result.current.loading).toEqual(false);
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual({ message: 'success!' });
+  });
+
+  it('should correctly handle an error response', async () => {
+    const mockedFetch = globalThis.fetch as ReturnType<typeof vi.fn>;
+    mockedFetch.mockResolvedValueOnce({
+      ok: false,
+      json: async () => ({ error: 'something went wrong' }),
+    });
+    const { result } = renderHook(() =>
+      useMutationRequest<{ field1: string; field2: string }, Record<string, string>>(
+        'dummy-endpoint',
+        'POST',
+      ),
+    );
+    await act(async () =>
+      result.current.sendRequest({ field1: 'some data', field2: 'other data' }),
+    );
+    expect(result.current.loading).toEqual(false);
+    expect(result.current.data).toBeNull();
+    expect(result.current.error?.message).toEqual('something went wrong');
+  });
+});
