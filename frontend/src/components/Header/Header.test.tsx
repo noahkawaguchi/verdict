41919788a8cd2316b2c5c9c40edbc2cc21e8528c
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-03-23 12:04:34 -0400

    Write tests for Header component

--- frontend/src/components/Header/Header.test.tsx
+++ frontend/src/components/Header/Header.test.tsx
@@ -1,14 +1,44 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { userEvent } from '@testing-library/user-event';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '../../pages/HomePage/HomePage';
+import CreatePollPage from '../../pages/CreatePoll/CreatePollPage';
+import CastBallotPage from '../../pages/CastBallot/CastBallotPage';
+import ViewResultsPage from '../../pages/ViewResults/ViewResultsPage';
+import NotFoundPage from '../../pages/NotFoundPage';
 import Header from './Header';
 
 describe('Header', () => {
-  it('should render', () => {
+  beforeEach(() =>
     render(
-      <MemoryRouter>
+      <MemoryRouter initialEntries={['/']}>
         <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/create-poll' element={<CreatePollPage />} />
+          <Route path='/cast-ballot/:pollId?' element={<CastBallotPage />} />
+          <Route path='/view-results/:pollId?' element={<ViewResultsPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
       </MemoryRouter>,
-    );
-    expect(screen.getByText('Verdict')).toBeInTheDocument();
+    ),
+  );
+
+  it('should render', () => expect(screen.getByText('Verdict')).toBeInTheDocument());
+
+  it('should redirect to the correct page', async () => {
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText('Create Poll'));
+    expect(screen.getByText('Create a New Poll')).toBeInTheDocument();
+
+    await user.click(screen.getByText('Cast Ballot'));
+    expect(screen.getByText('Cast a New Ballot')).toBeInTheDocument();
+
+    await user.click(screen.getByText('View Results'));
+    expect(screen.getByText("View a Poll's Results")).toBeInTheDocument();
+
+    await user.click(screen.getByText('Verdict'));
+    expect(screen.getByText('Welcome to Verdict')).toBeInTheDocument();
   });
 });
